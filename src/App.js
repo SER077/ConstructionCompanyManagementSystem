33bf3427f79d4f47dadc493248b97c613e6d0911
lkh-krishen <lkhkrishen@gmail.com>
2022-09-06 21:47:58 +0530

    Essentail files and folders created & React App working

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <h1>
+            Welcome to React App thats build using Webpack and Babel separately
+        </h1>
+        
+    )
+}
+
+export default App
